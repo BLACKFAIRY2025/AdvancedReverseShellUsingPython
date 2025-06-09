ec8cc40689a674ee75584f0056aebd1b530f3603
BLACKFAIRY2025 <poojanokhwal04@gmail.com>
2025-06-09 20:38:57 +0530

    Advanced Reverse Shell

---
+++ AdvancedReverseShell/advancedreverseshell.py
@@ -0,0 +1,16 @@
+import socket
+import subprocess
+
+
+def shell():
+  command=input("Command: " %str(ip))
+
+def server():
+  global s
+  global ip
+  global target
+  s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.bind(("192.168.0.1", 4444))
+  s.listen(5)
+
